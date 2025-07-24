cae70be56bcb5d9ed274d5c66e014b2b48d96be4
noctali168 <r122318@g.kobe-kosen.ac.jp>
2025-07-24 11:36:30 +0900

    dist追加

--- src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'    // 追加：ルーターをインポート
-import './style.css'
-
-createApp(App).use(router).mount('#app')   // routerを使うように追加
